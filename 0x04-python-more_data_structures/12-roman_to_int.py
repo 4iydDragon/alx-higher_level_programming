49089c06476413dce0562c78b1210cdffec9d600
4iydDragon <emmanuel.k.kangogo@gmail.com>
2023-08-16 18:40:29 +0300

    fixing 6 10 and 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
+    if roman_string is None or type(roman_string) is not str:
         return 0
-
-    if not isinstance(roman_string, str):
+    if roman_string == "":
         return 0
-
-    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(rom_num.keys())
-
     num = 0
-    last_rom = 0
-    list_num = [0]
-
-    for ch in roman_string:
-        for r_num in list_keys:
-            if r_num == ch:
-                if rom_num.get(ch) <= last_rom:
-                    num += to_subtract(list_num)
-                    list_num = [rom_num.get(ch)]
-                else:
-                    list_num.append(rom_num.get(ch))
-
-                last_rom = rom_num.get(ch)
-
-    num += to_subtract(list_num)
-
-    return (num)
+    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    for i, j in zip(roman_string, roman_string[1:]):
+        if i not in dic.keys():
+            return 0
+        elif dic[i] >= dic[j]:
+            num += dic[i]
+        else:
+            num -= dic[i]
+    num += dic[roman_string[-1]]
+    return num
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    list_order = list(a_dictionary.keys())
-    list_order.sort()
-    for i in list_order:
-        print("{}: {}".format(i, a_dictionary.get(i)))
+    for key, value in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, value))
         
