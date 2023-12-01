ddb395d51c1bf579cefe3943be161e7cde8e467f
Emmanuel <emmanuel.k.kangogo@gmail.com>
2023-12-01 05:43:03 -0800

    checker

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
 """
-Script that takes -Id value in the response header.
-"""
-
-import urllib.request
 import sys
+import urllib.request
 
 if __name__ == "__main__":
-    # Check if a URL is provided as a command-line argument
-    if len(sys.argv) != 2:
-        print("Usage: {} <URL>".format(sys.argv[0]))
-        sys.exit(1)
-
     url = sys.argv[1]
 
-    try:
-        with urllib.request.urlopen(url) as response:
-
-            x_request_id = response.getheader('X-Request-Id')
-            print(x_request_id)
-    except urllib.error.URLError as e:
-        print("Error:
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
